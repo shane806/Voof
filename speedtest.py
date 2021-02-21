0a690736585adb2e59f1107f86ce3cb9d27fc9f5
Shane Humphrey <tu06379@umbc.edu>
2021-02-21 13:51:51 -0500

    Add files via upload

--- speedtest.py
+++ speedtest.py
@@ -8,10 +8,12 @@ Created on Thu Jan 21 05:45:52 2021
 import cv2 as cv
 import numpy as np
 import torch
+from tqdm import tqdm
 # from tinygrad.tensor import Tensor
 import torchvision.transforms as T
-from tqdm import tqdm
+ 
 
+# from cv2 import waitKey
 
 def draw_flow(img, flow, step=16):
     h, w = img.shape[:2]
@@ -40,17 +42,18 @@ def generate_frames(video):
 
 def transform(image, bright_factor):
     """augment brightness, crop/resize"""
-
-    hsv_image = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+    
+    resized_image = cv.resize(image[100:440, :-90], (220, 66),
+                              interpolation=cv.INTER_AREA)
+    
+    hsv_image = cv.cvtColor(resized_image, cv.COLOR_BGR2HSV)
 
     hsv_image[:, :, 2] = hsv_image[:, :, 2] * bright_factor
 
     image_rgb = cv.cvtColor(hsv_image, cv.COLOR_HSV2BGR)
 
-    resized_image = cv.resize(image_rgb[100:440, :-90,:], (330, 99),
-                              interpolation=cv.INTER_AREA)
 
-    return resized_image
+    return image_rgb
 
 
 def generate_optical_flow_dataset(mp4_path, text_path):
@@ -58,102 +61,101 @@ def generate_optical_flow_dataset(mp4_path, text_path):
 
     # (height, width, number_of_channels) = (480, 640, 3)
     video = cv.VideoCapture(mp4_path)
-    ret, prev = video.read()
-    t = 0
-    show_hsv = False
-    show_rgb = False
+    # ret, prev_frame = video.read()
+    # t = 0
+    # show_hsv = False
+    # show_rgb = False
 
-    while ret:
+    # while ret:
 
-        ret, curr = video.read()
+        # ret, curr_frame = video.read()
 
-        if ret:
+        # if ret:
 
             # yield ret, prev_frame, curr_frame
 
-    # for t, (ret, prev_frame, curr_frame) in enumerate(tqdm(generate_frames(video),
-    #                                                        desc='Generating dense optical flow tensors')):
+    for t, (ret, prev_frame, curr_frame) in enumerate(tqdm(generate_frames(video),
+                                                            desc='Generating dense optical flow tensors')):
 
-            bright_factor = 0.2 + np.random.uniform()
-    
-            prev_frame, curr_frame = transform(prev, bright_factor), \
-                                     transform(curr, bright_factor)
-    
-            prev_gray, curr_gray = cv.cvtColor(prev_frame, cv.COLOR_BGR2GRAY), \
-                                   cv.cvtColor(curr_frame, cv.COLOR_BGR2GRAY)
-    
-    
-            flow = cv.calcOpticalFlowFarneback(prev_gray, curr_gray,
-                                               None, 0.5, 1, 15, 2, 5, 1.3, 0)
-    
-            mag, ang = cv.cartToPolar(flow[..., 0], flow[..., 1])
-            
-            hsv = np.zeros_like(prev_frame)
-    
-            hsv[:, :, 0] = ang * (180 / np.pi / 2)
+        bright_factor = 0.2 + np.random.uniform()
 
-            hsv[:, :, 1] = 255
-    
-            hsv[:, :, 2] = cv.normalize(mag, None, 0, 255, cv.NORM_MINMAX)
-    
-            rgb_flow = cv.cvtColor(hsv, cv.COLOR_HSV2RGB)
-    
-            ch = cv.waitKey(15)
-    
-            if (ch == ord('q')) or not ret:
-                
-                cv.destroyAllWindows()
-    
-                video.release()
-    
-                break
-    
-            # if ch == ord('1'):
-            cv.imshow('frame', curr_frame)
-            # cv.imshow('Optical flow', draw_flow(curr_gray, flow))
-    
-            if show_hsv:
-                cv.imshow('hsv', hsv)
-    
-            if show_rgb:
-    
-                cv.imshow('rgb_flow', rgb_flow)
-    
-            if ch == ord('1'):
-    
-                show_rgb = not show_rgb
-    
-                print('RGB flow visualization is', ['off', 'on'][show_hsv])
-    
-            if ch == ord('2'):
-                show_hsv = not show_hsv
-    
-                print('HSV flow visualization is', ['on', 'off'][show_hsv])
-    
-            rgb_flow_tensor = T.ToTensor()(rgb_flow).unsqueeze(0)
-    
-            # if not t:
-            #     flow_stack = rgb_flow_tensor
-    
-            # else:
+        prev_frame, curr_frame = transform(prev_frame, bright_factor), \
+                                 transform(curr_frame, bright_factor)
+
+        prev_gray, curr_gray = cv.cvtColor(prev_frame, cv.COLOR_BGR2GRAY), \
+                               cv.cvtColor(curr_frame, cv.COLOR_BGR2GRAY)
+
+
+        flow = cv.calcOpticalFlowFarneback(prev_gray, curr_gray,
+                                           None, 0.5, 1, 15, 2, 5, 1.3, 0)
+
+        mag, ang = cv.cartToPolar(flow[..., 0], flow[..., 1])
+        
+        hsv = np.zeros_like(prev_frame)
+
+        hsv[:, :, 0] = ang * (180 / np.pi / 2)
+
+        hsv[:, :, 1] = 255
+
+        hsv[:, :, 2] = cv.normalize(mag, None, 0, 255, cv.NORM_MINMAX)
+
+        rgb_flow = cv.cvtColor(hsv, cv.COLOR_HSV2RGB)
+
+        ch = cv.waitKey(15)
+
+        if (ch == ord('q')) or not ret:
             
-            #     flow_stack = torch.cat([flow_stack, rgb_flow_tensor])
-    
-            prev = curr
-        t += 1
+            cv.destroyAllWindows()
+
+            video.release()
+
+            break
+
+        # if ch == ord('1'):
+        cv.imshow('frame', curr_frame)
+        # cv.imshow('Optical flow', draw_flow(curr_gray, flow))
+
+        # if show_hsv:
+        #     cv.imshow('hsv', hsv)
+
+        # if show_rgb:
+
+        #     cv.imshow('rgb_flow', rgb_flow)
+
+        # if ch == ord('1'):
+
+        #     show_rgb = not show_rgb
+
+        #     print('RGB flow visualization is', ['off', 'on'][show_hsv])
+
+        # if ch == ord('2'):
+        #     show_hsv = not show_hsv
+
+        #     print('HSV flow visualization is', ['on', 'off'][show_hsv])
+
+        rgb_flow_tensor = T.ToTensor()(rgb_flow).unsqueeze(0)
+
+        if not t:
+            flow_stack = rgb_flow_tensor
+
+        else:
+        
+            flow_stack = torch.cat([flow_stack, rgb_flow_tensor])
+
+        prev_frame = curr_frame
+        # t += 1
 
     # can't estimate speed of first frame
-    # speed_vector = np.loadtxt(text_path)[1:]
-    # flow_dataset = torch.utils.data.TensorDataset(flow_stack,
-    #                                               torch.from_numpy(speed_vector).float())
-    # return flow_dataset
+    speed_vector = np.loadtxt(text_path)[1:]
+    flow_dataset = torch.utils.data.TensorDataset(flow_stack,
+                                                  torch.from_numpy(speed_vector).float())
+    return flow_dataset
 
 
 def save_whole_set(mp4_path, text_path, save_path):
     """save whole dataset"""
-    # flow_dataset = \
-    generate_optical_flow_dataset(mp4_path, text_path)
-    # torch.save(flow_dataset, save_path)
+    flow_dataset = generate_optical_flow_dataset(mp4_path, text_path)
+    torch.save(flow_dataset, save_path)
 
 
 if __name__ == '__main__':
